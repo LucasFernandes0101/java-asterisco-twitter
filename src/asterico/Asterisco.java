e5cd9f62317245ebb196e7c309bada35d4318c94
LucasFernandes0101 <fernandeslucas451@gmail.com>
2020-07-13 20:57:43 -0300

    editado

--- src/asterico/Asterisco.java
+++ src/asterico/Asterisco.java
@@ -252,3 +252,4 @@ public class Asterisco extends javax.swing.JFrame {
     private javax.swing.JTextField txt_texto;
     // End of variables declaration//GEN-END:variables
 }
+//editado by mussalam
